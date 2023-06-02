4de0bab3617beaf38047f45d7717a2c315b3434b
ShockOfWave <shockofwave90@gmail.com>
2023-06-02 11:09:52 +0300

    App first version

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -17,3 +17,22 @@ def load_sc():
         f.close()
     
     return sc
+
+def load_les():
+    with open(os.path.join(get_project_path(), 'models', 'preprocessing', 'le_dob.pkl'), 'rb') as f:
+        le_dob = pickle.load(f)
+        f.close()
+    
+    with open(os.path.join(get_project_path(), 'models', 'preprocessing', 'le_donor.pkl'), 'rb') as f:
+        le_donor = pickle.load(f)
+        f.close()
+        
+    with open(os.path.join(get_project_path(), 'models', 'preprocessing', 'le_method.pkl'), 'rb') as f:
+        le_method = pickle.load(f)
+        f.close()
+        
+    with open(os.path.join(get_project_path(), 'models', 'preprocessing', 'le_prec.pkl'), 'rb') as f:
+        le_prec = pickle.load(f)
+        f.close()  
+    
+    return le_dob, le_donor, le_method, le_prec
